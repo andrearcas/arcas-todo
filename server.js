830cffbd253e8ad53dfcc1738bb0e2f089790c88
André Arcas <andre.luiz.arcas@gmail.com>
2016-07-18 05:14:44 -0300

    Added find todo route

--- server.js
+++ server.js
@@ -2,8 +2,43 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos =[{
+	id: 1,
+	description: 'Remake Dalva\'s script',
+	completed: false
+}, {
+	id: 2,
+	description: 'Workout',
+	completed: false
+}, {
+	id: 3,
+	description: 'Meeting with Taty',
+	completed: true
+}];
+
 app.get('/', function (req, res) {
 	res.send('Todo API root');
+});
+
+app.get('/todos', function (req, res) {
+	res.json(todos);
+});
+
+app.get('/todos/:id', function (req, res) {
+	var todoID = parseInt(req.params.id, 10);
+	var matchedTodo;
+
+	todos.forEach(function (todo) {
+		if (todo.id === todoID) {
+			matchedTodo = todo;
+		}
+	});
+
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send('The page was not found');
+	}
 });
 
 app.listen(PORT, function () {
