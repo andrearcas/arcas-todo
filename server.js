ed40ae3352c7fe485fb0af037e7f45a77ad45381
André Arcas <andre.luiz.arcas@gmail.com>
2016-07-21 20:31:54 -0300

    Add POST for To do's

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos =[{
-	id: 1,
-	description: 'Remake Dalva\'s script',
-	completed: false
-}, {
-	id: 2,
-	description: 'Workout',
-	completed: false
-}, {
-	id: 3,
-	description: 'Meeting with Taty',
-	completed: true
-}];
+var todos =[];
+var todoNextId= 1;  
+
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Todo API root');
@@ -39,6 +33,17 @@ app.get('/todos/:id', function (req, res) {
 	} else {
 		res.status(404).send('The page was not found');
 	}
+});
+
+// POST
+app.post( '/todos', function (req, res) {
+	var body = req.body;
+
+	body.id = todoNextId++;
+	todos.push(body);
+
+	
+	res.json(body);
 });
 
 app.listen(PORT, function () {
