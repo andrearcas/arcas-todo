54c3c4a151edaa5338853e4bf1ace375179b3d55
André Arcas <andre.luiz.arcas@gmail.com>
2016-07-21 21:20:05 -0300

    Refactor API with _

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -20,13 +20,7 @@ app.get('/todos', function (req, res) {
 
 app.get('/todos/:id', function (req, res) {
 	var todoID = parseInt(req.params.id, 10);
-	var matchedTodo;
-
-	todos.forEach(function (todo) {
-		if (todo.id === todoID) {
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoID});
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -37,7 +31,14 @@ app.get('/todos/:id', function (req, res) {
 
 // POST
 app.post( '/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body,'id', 'description', 'completed');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
+
 
 	body.id = todoNextId++;
 	todos.push(body);
