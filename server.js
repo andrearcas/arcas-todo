b221fab5c6bf2931a89a53ccc00dfba3a01cabee
André Arcas <andre.luiz.arcas@gmail.com>
2016-07-26 17:39:43 -0300

    Add PUT todos/:id

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/todos/:id', function (req, res) {
 
 // POST
 app.post( '/todos', function (req, res) {
-	var body = _.pick(req.body,'id', 'description', 'completed');
+	var body = _.pick(req.body, 'description', 'completed');
 
 	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
 		return res.status(400).send();
@@ -57,10 +57,52 @@ app.delete('/todos/:id', function (req, res) {
 	} else {
 		res.status(404).send('To do doesn\'t exist');
 	}
+});
 
+app.put('/todos/:id', function (req, res) {
+	var todoID = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoID});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+	
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
 
-})
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	} 
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+
+});
 
 app.listen(PORT, function () {
 	console.log('Express listening on PORT ' + PORT);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
