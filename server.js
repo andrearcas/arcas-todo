f163a0d055c5d7c3731b7b2dcff2e88c56c8caff
André Arcas <andre.luiz.arcas@gmail.com>
2016-07-29 19:14:21 -0300

    Add POST Sequelize

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-
 var todos = [];
 var todoNextId = 1;
 
@@ -56,18 +57,26 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
-
-	body.description = body.description.trim();
-
-
-	body.id = todoNextId++;
-	todos.push(body);
+	db.todo.create(body).then(function (task) {
+		res.json(task.toJSON());
+	}).catch(function (e) {
+		res.status(400).json(e);
+	});
 
 
-	res.json(body);
+
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).send();
+	// }
+
+	// body.description = body.description.trim();
+
+
+	// body.id = todoNextId++;
+	// todos.push(body);
+
+
+	// res.json(body);
 });
 
 app.delete('/todos/:id', function(req, res) {
@@ -113,6 +122,8 @@ app.put('/todos/:id', function(req, res) {
 
 });
 
-app.listen(PORT, function() {
-	console.log('Express listening on PORT ' + PORT);
+db.sequelize.sync().then(function () {
+	app.listen(PORT, function() {
+		console.log('Express listening on PORT ' + PORT);		
+	})
 });
